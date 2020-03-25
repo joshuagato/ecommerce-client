04838aef08c2874172c1895a956b41066e1b34fd
Joshua Gato <joshuagatogato37@gmail.com>
2020-03-25 10:50:07 +0000

    'update'

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import './NavBar.scss';
 
 import { connect } from 'react-redux';
 
-// import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
@@ -13,78 +13,72 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Logo from '../../../src/assets/img/logo.png';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserAlt, faCartPlus, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faUserAlt, faShoppingCart, faKey } from '@fortawesome/free-solid-svg-icons';
 
 export class NavBar extends Component {
 
-    collapse = () => {
-        this.isCollapsed = true;
-    }
+  collapse = () => {
+      this.isCollapsed = true;
+  }
     
-    collpseHandler = () => {
-        this.isCollapsed = !this.isCollapsed;
-    }
-    
-    closeDropdown = dropdown => {
-        dropdown.close();
-    }
-    
-    logout = () => {}
+  collpseHandler = () => {
+      this.isCollapsed = !this.isCollapsed;
+  }
 
-    search = event => {    
-        this.setState({ searchTerm: event.target.value });
-        
-        if(event.keyCode === 13) console.log("Hi there, " + event.target.value);
+  closeDropdown = dropdown => {
+      dropdown.close();
+  }
 
-        if(event.target.name === 'searchBtn') console.log("Hi there, " + event.target.value);
-    }
+  logout = () => {}
 
-    componentDidUpdate() {
-        // console.log(this.props.name)
-    }
+  search = event => {
+    this.setState({ searchTerm: event.target.value });
+
+    if(event.keyCode === 13) console.log("Hi there, " + event.target.value);
+
+    if(event.target.name === 'searchBtn') console.log("Hi there, " + event.target.value);
+  }
+
+  componentDidUpdate() {
+    // console.log(this.props.name)
+  }
        
-    render() {
-        return (
-            <Navbar id="rbNavbar" expand="lg">
-                <Navbar.Brand href="/">
-                    <img src={Logo} alt='logo' width='50' />Amazon
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto ml-5">
-                        <Nav.Link href="/categories">Categories</Nav.Link>
-                            {
-                                this.props.name ? <NavDropdown title={this.props.name} id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/profile">
-                                                <FontAwesomeIcon icon={faUserAlt} /> Profile
-                                            </NavDropdown.Item>
-                                            <NavDropdown.Item href="/logout">
-                                                <FontAwesomeIcon icon={faKey} /> Logout
-                                            </NavDropdown.Item>
-                                        </NavDropdown> :
-                                <Nav.Link href="/login">
-                                    Login
-                                </Nav.Link>
-                            }
-                        <Nav.Link href="/cart">
-                            <FontAwesomeIcon icon={faCartPlus} /> Cart
-                        </Nav.Link>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="search" placeholder="Search" id="searchInput" />
-                        <Button variant="success" id="searchBtn">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Navbar id="rbNavbar" expand="lg">
+        <Navbar.Brand>
+          <NavLink to='/' className="nav-link" role="button"><img src={Logo} alt='logo' width='50' />Amazon</NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto ml-5">
+            <NavLink to="/categories" className="nav-link" role="button">Categories</NavLink>
+              {
+                this.props.name ? <NavDropdown title={this.props.name} id="basic-nav-dropdown">
+                  <NavLink to='/profile' className="dropdown-item nav-link" role="button"><FontAwesomeIcon icon={faUserAlt} /> Profile</NavLink>
+                  <NavLink to='/logout' className="dropdown-item nav-link" role="button"><FontAwesomeIcon icon={faKey} /> Logout</NavLink>
+              </NavDropdown> :
+                <NavLink to='/login' className="nav-link" role="button">Login</NavLink>
+              }
+            <NavLink to='/cart' className="nav-link" role="button">
+              <FontAwesomeIcon icon={faShoppingCart} /> Cart
+            </NavLink>
+          </Nav>
+          <Form inline>
+            <FormControl type="search" placeholder="Search" id="searchInput" />
+            <Button variant="success" id="searchBtn">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        name: state.loggedUserReducer.personalDetails.name,
-        token: state.loginReducer.token,
-    };
+  return {
+    name: state.loggedUserReducer.personalDetails.name,
+    token: state.loginReducer.token,
+  };
 }
 
 export default connect(mapStateToProps)(NavBar);
